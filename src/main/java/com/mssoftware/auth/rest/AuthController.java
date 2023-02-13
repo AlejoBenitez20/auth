0bfe8d1a7ebb23c64de2315d0cfbe3644d493167
Your Name <you@example.com>
2023-02-12 19:00:25 -0500

    chage apellide 3

--- src/main/java/com/mssoftware/auth/rest/AuthController.java
+++ src/main/java/com/mssoftware/auth/rest/AuthController.java
@@ -15,7 +15,7 @@ public class AuthController implements AuthApi {
     public ResponseEntity<Persona> getDataAuth() {
         Persona persona = new Persona();
         persona.setNombre("Samelo");
-        persona.setApellido("Rodriguez");
+        persona.setApellido("Rodriguez2");
 
         return new ResponseEntity<>(persona , HttpStatus.OK);
     }
