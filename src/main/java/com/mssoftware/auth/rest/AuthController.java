ae21c82770a2f8c6318bc6c45a317df7933e880b
Your Name <you@example.com>
2023-02-13 20:55:02 -0500

    Cambia apellido Aldana

--- src/main/java/com/mssoftware/auth/rest/AuthController.java
+++ src/main/java/com/mssoftware/auth/rest/AuthController.java
@@ -15,7 +15,7 @@ public class AuthController implements AuthApi {
     public ResponseEntity<Persona> getDataAuth() {
         Persona persona = new Persona();
         persona.setNombre("Samelo");
-        persona.setApellido("Aldana35");
+        persona.setApellido("Aldana36");
 
         return new ResponseEntity<>(persona , HttpStatus.OK);
     }
