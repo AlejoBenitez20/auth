62ff2786285b1ffa519400dfed22905b95bb2762
Your Name <you@example.com>
2023-02-14 18:46:29 -0500

    Cambia apellido Rodriguez100

--- src/main/java/com/mssoftware/auth/rest/AuthController.java
+++ src/main/java/com/mssoftware/auth/rest/AuthController.java
@@ -4,18 +4,15 @@ import com.mssoftware.auth.api.AuthApi;
 import com.mssoftware.auth.api.domain.Persona;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController implements AuthApi {
 
-
     public ResponseEntity<Persona> getDataAuth() {
         Persona persona = new Persona();
         persona.setNombre("Samelo");
-        persona.setApellido("Aldana50");
+        persona.setApellido("Rodriguez100");
 
         return new ResponseEntity<>(persona , HttpStatus.OK);
     }
